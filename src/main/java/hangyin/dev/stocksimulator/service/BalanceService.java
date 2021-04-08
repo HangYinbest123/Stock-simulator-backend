996ac68b2f8d701fd7dcdd70379b3ddd78ac4064
hang yin <hy2568@columbia.edu>
2021-04-07 21:53:47 -0500

    Record user's total direct deposit

--- src/main/java/hangyin/dev/stocksimulator/service/BalanceService.java
+++ src/main/java/hangyin/dev/stocksimulator/service/BalanceService.java
@@ -1,11 +1,13 @@
 package hangyin.dev.stocksimulator.service;
 
 import hangyin.dev.stocksimulator.dto.AddBalanceRequest;
+import hangyin.dev.stocksimulator.dto.balance.AddBalanceReason;
 import hangyin.dev.stocksimulator.entity.User;
 import hangyin.dev.stocksimulator.entity.UserBalance;
 import hangyin.dev.stocksimulator.repository.UserBalanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Service
 public class BalanceService {
@@ -33,9 +35,13 @@ public class BalanceService {
         }
     }
 
-    public UserBalance addUserBalance(String userId, AddBalanceRequest addBalanceRequest) throws Exception {
+    public UserBalance addUserBalance(String userId, AddBalanceRequest addBalanceRequest, AddBalanceReason reason) throws Exception {
         UserBalance userBalance = getUserBalanceByUserId(userId);
         double updatedBalanceAmount = userBalance.getBalance() + addBalanceRequest.getAmount();
+        if(reason == AddBalanceReason.DirectDeposit){
+            double updatedDirectDeposit = userBalance.getDirectDeposit() + addBalanceRequest.getAmount();
+            userBalance.setDirectDeposit(updatedDirectDeposit);
+        }
         if(updatedBalanceAmount < 0){
             throw new Exception("updated balance can't be less than 0: ");
         }else{
